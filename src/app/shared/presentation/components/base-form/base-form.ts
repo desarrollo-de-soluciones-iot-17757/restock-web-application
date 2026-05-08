1ac30de5797ce40dffb7fe42b2703f093d088613
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-08 17:45:33 -0500

    chore: initial commit

---
+++ src/app/shared/presentation/components/base-form/base-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-form',
+  standalone: false,
+  templateUrl: './base-form.html',
+  styleUrl: './base-form.css',
+})
+export class BaseForm {}
