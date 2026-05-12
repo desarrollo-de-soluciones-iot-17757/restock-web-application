24f0100481c298720f7507e2dec55abc5ad8f5a1
jahazielgg <jahazielgpz@gmail.com>
2026-05-12 14:36:13 -0500

    feat: add profiles routing and registration business details component.

---
+++ src/app/profiles/presentation/profiles.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const profilesRoutes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./views/registration-personal-profile/registration-personal-profile').then(
+        (m) => m.RegistrationPersonalProfile
+      ),
+    title: 'Create your Account',
+  },
+];
