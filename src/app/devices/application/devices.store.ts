80380c6018df230fee791c25734b9a698c7a33b2
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-12 18:56:16 -0500

    feat(devices-list): add device registration and retrieval functionality.

--- src/app/devices/application/devices.store.ts
+++ src/app/devices/application/devices.store.ts
@@ -1,8 +1,9 @@
-import {Injectable, signal} from '@angular/core';
+import {computed, Injectable, Signal, signal} from '@angular/core';
 import {Device} from '../domain/model/device.entity';
 import {DevicesApi} from '../infrastructure/devices-api';
 import {RegisterDeviceCommand} from '../domain/model/register-device.command';
 import {retry} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 /**
  * Application service managing Device Management domain state and orchestration.
@@ -19,21 +20,45 @@ import {retry} from 'rxjs';
 @Injectable({providedIn: "root"})
 export class DevicesStore {
 
-  private readonly devicesSignal = signal<Device[]>([]);
+  /**
+   * Signal representing the list of registered devices.
+   * @private
+   */
+  private readonly _devicesSignal = signal<Device[]>([]);
 
-  readonly devices = this.devicesSignal.asReadonly();
+  /**
+   * Readonly signal representing the loading status of the device registration process.
+   */
+  readonly devices = this._devicesSignal.asReadonly();
 
-  private readonly loadingSignal = signal<boolean>(false);
+  /**
+   * Signal representing the loading status of the device registration process.
+   * @private
+   */
+  private readonly _loadingSignal = signal<boolean>(false);
 
-  readonly loading = this.loadingSignal.asReadonly();
+  /**
+   * Readonly signal representing the loading status of the device registration process.
+   */
+  readonly loading = this._loadingSignal.asReadonly();
 
-  private readonly errorSignal = signal<string | null>(null);
+  /**
+   * Signal representing the error message if any occurred during the device registration process.
+   * @private
+   */
+  private readonly _errorSignal = signal<string | null>(null);
 
-  readonly error = this.errorSignal.asReadonly();
+  /**
+   * Readonly signal representing the error message if any occurred during the device registration process.
+   */
+  readonly error = this._errorSignal.asReadonly();
 
-  constructor(private devicesApi: DevicesApi) {
-
-  }
+  /**
+   * Creates an instance of DevicesStore.
+   *
+   * @param devicesApi - The DevicesApi service used for communication with the remote system.
+   */
+  constructor(private devicesApi: DevicesApi) {}
 
   /**
    * Formats error messages for display to users or logs.
@@ -52,24 +77,59 @@ export class DevicesStore {
   }
 
   /**
+   * Loads the list of devices for a specific account.
+   *
+   * @param accountId - The unique identifier of the account to load devices for.
+   */
+  loadDevicesForAccount(accountId: string): void {
+    this._loadingSignal.set(true);
+    this._errorSignal.set(null);
+    this.devicesApi.getDevicesByAccountId(accountId)
+      .pipe(takeUntilDestroyed())
+      .subscribe({
+        next: devices => {
+          console.log(devices);
+          this._devicesSignal.set(devices);
+          this._loadingSignal.set(false);
+          this._errorSignal.set(null);
+        },
+        error: err => {
+          this._loadingSignal.set(false);
+          this._errorSignal.set(this.formatError(err, 'Failed to load devices'));
+        }
+      })
+  }
+
+  /**
+   * Gets a signal representing the device with the specified ID.
+   *
+   * @param id - The unique identifier of the device to retrieve.
+   * @returns A signal representing the device with the specified ID, or undefined if not found.
+   */
+  getDeviceById(id: string): Signal<Device | undefined> {
+    return computed(() => id
+      ? this.devices().find(device => device.id === id) : undefined);
+  }
+
+  /**
    * Registers a new device to the store and remote system.
    *
    * @param registerDeviceCommand - The command containing the device registration data.
    */
   registerDevice(registerDeviceCommand: RegisterDeviceCommand): void {
-    this.loadingSignal.set(true);
-    this.errorSignal.set(null);
+    this._loadingSignal.set(true);
+    this._errorSignal.set(null);
     this.devicesApi.registerDevice(registerDeviceCommand)
       .pipe(retry(2))
       .subscribe({
         next: registeredDevice => {
-          this.devicesSignal.update(devices => [...devices, registeredDevice]);
-          this.loadingSignal.set(false);
-          this.errorSignal.set(null);
+          this._devicesSignal.update(devices => [...devices, registeredDevice]);
+          this._loadingSignal.set(false);
+          this._errorSignal.set(null);
         },
         error: err => {
-          this.loadingSignal.set(false);
-          this.errorSignal.set(this.formatError(err, 'Failed to register device'));
+          this._loadingSignal.set(false);
+          this._errorSignal.set(this.formatError(err, 'Failed to register device'));
         }
       });
   }
