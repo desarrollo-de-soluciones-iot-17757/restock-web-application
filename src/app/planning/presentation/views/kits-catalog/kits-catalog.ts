925ac31aa9dbf137016afd23bba40992dc54c444
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2026-05-29 01:21:33 -0500

    feat(kits): enhance kits catalog layout and functionality with improved styling and edit modal integration

--- src/app/planning/presentation/views/kits-catalog/kits-catalog.ts
+++ src/app/planning/presentation/views/kits-catalog/kits-catalog.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { KitCardComponent } from '../../components/kit-card/kit-card';
 import { KitsStore } from '../../../application/kits.store';
 import { Kit } from '../../../domain/model/kit.entity';
 import { KitFormModalComponent } from '../recipe-form/recipe-form';
+import { KitCardComponent } from '../../components/kit-cad-1/kit-cad-1';
+import { EditKitModalComponent } from '../edit-kit/edit-kit';
 
 @Component({
   selector: 'app-planning-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule, KitCardComponent, KitFormModalComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    KitCardComponent,
+    KitFormModalComponent,
+    EditKitModalComponent,
+  ],
   templateUrl: './kits-catalog.html',
   styleUrl: './kits-catalog.css',
 })
@@ -17,6 +24,8 @@ export class PlanningDashboardComponent implements OnInit {
   protected readonly kitsStore = inject(KitsStore);
 
   isCreateModalOpen = signal(false);
+  isEditModalOpen = signal(false);
+  selectedKit = signal<Kit | null>(null);
 
   ngOnInit(): void {
     console.log('Iniciando carga de kits...');
@@ -32,5 +41,12 @@ export class PlanningDashboardComponent implements OnInit {
   }
   handleEditKit(kit: Kit): void {
     console.log('Kit seleccionado para actualización estratégica:', kit);
+    this.selectedKit.set(kit);
+    this.isEditModalOpen.set(true);
+  }
+
+  closeEditModal(): void {
+    this.isEditModalOpen.set(false);
+    this.selectedKit.set(null);
   }
 }
